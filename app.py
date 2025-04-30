732d45ebfbf248d2f42c6a0313a1759c1fb0c75c
Doleswari Saha <104640699+DOLESWARI@users.noreply.github.com>
2025-05-01 00:01:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import hashlib
 import sqlite3
-from streamlit_option_menu import option_menu
+
 
 import about_us
 import feedback
